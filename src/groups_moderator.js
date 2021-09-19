53ea699f8962b4b2f893183bc0257f281127ea87
Spidey03 <sayhai2naveenkumar@gmail.com>
2021-09-19 23:08:49 +0530

    Get group links of user as a moderator

--- src/groups_moderator.js
+++ src/groups_moderator.js
@@ -5,11 +5,12 @@ const playwright = require("playwright");
 
 (async() => {
 
-    //open browser
+    let group_urls = []
     let browserType = "chromium"
     const browser = await playwright[browserType].launch({
         headless: false
     });
+
 
     try {
         const credentials = ini.parse(fs.readFileSync('config/credentials.ini', 'utf-8'))["Credentials"];
@@ -23,13 +24,30 @@ const playwright = require("playwright");
         await page.fill('input[name=pass]', password);
         await page.press('input[name=pass]', 'Enter')
         await page.waitForNavigation();
+        await page.waitForResponse(response => {
+            return response.request().resourceType() === "xhr"
+        })
+
+        urls = await page.$$eval('div[data-visualcompletion=ignore-dynamic]>a', (elements) =>
+            elements.map((el) => el.href)
+        )
+        const hasGroupinURL = (str, groups) => {
+            str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").split(/\s+/).exclude(word);
+
+        }
+
+
+        for (let url of urls) {
+            if ((url.includes("feed") === false) && (url.includes("discover") === false) && (url.includes("notifications") === false) && (url.includes("groups") === true)) {
+                group_urls.push(url);
+            }
+        }
+        console.log(group_urls);
 
     } catch (error) {
         console.error(`Trying to run test on ${browserType}: ${error}`);
     } finally {
-        // await browser.close();
+        await browser.close();
     }
 
-
-    //close
 })();
