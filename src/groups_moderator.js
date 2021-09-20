8fb53bf3ea3d396eb9a7debd260718ef2cf53de3
Spidey03 <sayhai2naveenkumar@gmail.com>
2021-09-21 00:28:44 +0530

    navigate to profile of user

--- src/groups_moderator.js
+++ src/groups_moderator.js
@@ -28,17 +28,18 @@ const playwright = require("playwright");
             return response.request().resourceType() === "xhr"
         })
 
-        urls = await page.$$eval('div[data-visualcompletion=ignore-dynamic]>a', (elements) =>
-            elements.map((el) => el.href)
-        )
+        // urls = await page.$$eval('div[data-visualcompletion=ignore-dynamic]>a', (elements) =>
+        //     elements.map((el) => el.href)
+        // )
 
-        for (let url of urls) {
-            if ((url.includes("feed") === false) && (url.includes("discover") === false) && (url.includes("notifications") === false) && (url.includes("groups") === true)) {
-                group_urls.push(url);
-            }
-        }
-        console.log(group_urls);
-        group_url = group_urls[0];
+        // for (let url of urls) {
+        //     if ((url.includes("feed") === false) && (url.includes("discover") === false) && (url.includes("notifications") === false) && (url.includes("groups") === true)) {
+        //         group_urls.push(url);
+        //     }
+        // }
+        // console.log(group_urls);
+        // group_url = group_urls[0];
+        group_url = "https://www.facebook.com/groups/928052944589381/"
         page.goto(group_url.concat("people"));
         await page.waitForNavigation();
         await page.waitForResponse(response => {
@@ -50,10 +51,15 @@ const playwright = require("playwright");
             try {
                 let xpath = 'xpath=//div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div[4]/div/div/div/div/div/div/div/div/div/div/div[3]/div/div/div[2]/div[1]/div/div[2]/div/div[' + i + ']/div/div/div[1]/div/a';
                 await page.click(xpath);
-                console.log(page.url());
+                user_profile_url = page.url();
+                user_id = user_profile_url.split("/user/")[1];
+                console.log(user_id);
                 // validate isUser male with new page?
-
-                page.goBack();
+                newPage = await context.newPage();
+                let main_profile_url = "https://www.facebook.com/profile.php?id=";
+                await newPage.goto(main_profile_url.concat(user_id));
+                await newPage.close();
+                await page.goBack();
 
             } catch (error) {
                 console.log(error);
