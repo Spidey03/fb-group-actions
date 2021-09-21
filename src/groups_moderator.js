8919406e08cc836afc67e9267f3792d08f97e33b
Spidey03 <sayhai2naveenkumar@gmail.com>
2021-09-21 11:08:09 +0530

    group members

--- src/groups_moderator.js
+++ src/groups_moderator.js
@@ -1,3 +1,4 @@
+const { spawn } = require('child_process');
 const fs = require('fs');
 const ini = require('ini');
 const playwright = require("playwright");
@@ -28,45 +29,57 @@ const playwright = require("playwright");
             return response.request().resourceType() === "xhr"
         })
 
-        // urls = await page.$$eval('div[data-visualcompletion=ignore-dynamic]>a', (elements) =>
-        //     elements.map((el) => el.href)
-        // )
+        urls = await page.$$eval('div[data-visualcompletion=ignore-dynamic]>a', (elements) =>
+            elements.map((el) => el.href)
+        )
 
-        // for (let url of urls) {
-        //     if ((url.includes("feed") === false) && (url.includes("discover") === false) && (url.includes("notifications") === false) && (url.includes("groups") === true)) {
-        //         group_urls.push(url);
-        //     }
-        // }
-        // console.log(group_urls);
-        // group_url = group_urls[0];
-        group_url = "https://www.facebook.com/groups/928052944589381/"
-        page.goto(group_url.concat("people"));
-        await page.waitForNavigation();
-        await page.waitForResponse(response => {
-            return response.request().resourceType() === "xhr"
-        })
-
-        let i = 1;
-        while (i > 0) {
-            try {
-                let xpath = 'xpath=//div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div[4]/div/div/div/div/div/div/div/div/div/div/div[3]/div/div/div[2]/div[1]/div/div[2]/div/div[' + i + ']/div/div/div[1]/div/a';
-                await page.click(xpath);
-                user_profile_url = page.url();
-                user_id = user_profile_url.split("/user/")[1];
-                console.log(user_id);
-                // validate isUser male with new page?
-                newPage = await context.newPage();
-                let main_profile_url = "https://www.facebook.com/profile.php?id=";
-                await newPage.goto(main_profile_url.concat(user_id));
-                await newPage.close();
-                await page.goBack();
-
-            } catch (error) {
-                console.log(error);
-                break;
+        for (let url of urls) {
+            if ((url.includes("feed") === false) && (url.includes("discover") === false) && (url.includes("notifications") === false) && (url.includes("groups") === true)) {
+                group_urls.push(url);
             }
-            i++;
         }
+
+        for (group_url of group_urls) {
+            console.log(group_url)
+            page.goto(group_url.concat("people"));
+            await page.waitForNavigation();
+            await page.waitForResponse(response => {
+                return response.request().resourceType() === "xhr"
+            })
+
+            let i = 1;
+            while (i > 0) {
+                try {
+                    let xpath = 'xpath=//div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div[4]/div/div/div/div/div/div/div/div/div/div/div[3]/div/div/div[2]/div[1]/div/div[2]/div/div[' + i + ']/div/div/div[1]/div/a';
+                    await page.click(xpath);
+                    await page.waitForResponse(response => {
+                        return response.request().resourceType() === "xhr"
+                    })
+                    user_profile_url = await page.url();
+                    user_id = user_profile_url.split("/user/")[1];
+                    user_id = user_id.replace(new RegExp("/", "g"), "");
+
+                    // validate isUser male with new page?
+                    newPage = await context.newPage();
+                    let user_info_url = "https://www.facebook.com/profile.php?id=" + user_id + "&sk=about_contact_and_basic_info";
+                    await newPage.goto(user_info_url);
+                    await page.waitForResponse(response => {
+                            return response.request().resourceType() === "xhr"
+                        })
+                        // gender = await newPage.$$eval('span:has-text("Male")');
+                        // console.log(gender);
+
+                    await newPage.close();
+                    await page.goBack();
+
+                } catch (error) {
+                    console.log(error);
+                    break;
+                }
+                i++;
+            }
+        }
+
 
         console.log("Done");
 
